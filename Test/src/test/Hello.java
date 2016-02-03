f52aae8ababf29ed3e78c0d346d12d91ba20fbe0
malexon <malexson@gmail.com>
2016-02-03 18:44:33 -0500

    fda

--- Test/src/test/Hello.java
+++ Test/src/test/Hello.java
@@ -3,5 +3,7 @@ package test;
 public class Hello {
 	public static void main(String args[]){
 		System.out.println("hello world!");
+		System.out.println("how are you today?");
+		System.out.println("I am fine thank you");
 	}
 }
